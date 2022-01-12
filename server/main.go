e9e164cbae5f6ed7309670012f19b083f0720933
xAyumux <ayumu.f.1127@gmail.com>
2022-01-13 02:54:20 +0900

    APIの説明をコメントに追加

--- server/main.go
+++ server/main.go
@@ -8,12 +8,15 @@ import (
 func main() {
 	router := gin.Default()
 
-	// router.Use(middleware.RecordUaAndTime)
-
+	// idが一番大きいものを表示
 	router.GET("/get/poll", controller.GetPoll)
+	// 指定されたidを表示
 	router.GET("/poll", controller.GetQuery)
+	// title一覧を表示
 	router.GET("/list", controller.GetList)
+	// pollsテーブルに新しいデータを追加
 	router.POST("/post/polls", controller.PostPolls)
+	// votesテーブルに新しいデータを追加
 	router.POST("/post/votes", controller.PostVotes)
 
 	router.Run(":8000")
